f1da340b84921429f6f5c3ce79050bca94442b42
Gustavo Herrera <gustavoh.2312@gmail.com>
2019-06-11 19:12:55 -0500

    create services and model for get data

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Persona } from '../Modelo/Persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  Url = 'http://localhost:8080/personas';
+  // database crud_java_db, tabla: personas
+
+  getPersonas() {
+    return this.http.get<Persona>(this.Url);
+  }
+
 }
